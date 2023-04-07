575dfb38fc8b1f05c758500897d2dc4c6a15f861
Indra Madyasiwi W <madyasiwi@gmail.com>
2023-04-07 10:41:59 +0700

    Add scenes switchable using vs

---
+++ Assets/Scripts/AppStateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEditor;
+
+namespace Madyasiwi.Game {
+
+
+    public class AppStateController : MonoBehaviour {
+
+        [SerializeField]
+        public static string someValue;
+
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Init() {
+            // Instantiate self
+            var prefab = Resources.Load("Prefabs/AppStateController");
+            if (prefab != null) {
+                var instance = Instantiate(prefab, null, true);
+                instance.name = nameof(AppStateController);
+                DontDestroyOnLoad(instance);
+            }
+        }
+    }
+}
